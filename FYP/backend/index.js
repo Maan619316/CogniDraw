a214831f92df54b4ddd134ab735b3cf3d53816a2
Maan619316 <immaan619@gmail.com>
2024-06-11 06:09:22 +0500

    Update index.js

--- FYP/backend/index.js
+++ FYP/backend/index.js
@@ -6,7 +6,11 @@ const cors = require("cors");
 
 app.use(express.json());
 const corsOptions = {
-  origin: "https://cogni-draw-frontend-eight.vercel.app", // Update with your frontend URL
+   origin: [
+    "https://cogni-draw-frontend-eight.vercel.app",
+    "https://cogni-draw-frontend-n8phjdn34-usmans-projects-9557ae50.vercel.app",
+    // Add any other origins that you want to allow
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
